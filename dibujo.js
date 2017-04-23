7f37b12963019705d6e7512608e9433fad7f37b1
Mr Software <mistersoftware@users.noreply.github.com>
2017-04-23 13:06:17 -0500

    first commit

--- dibujo.js
+++ dibujo.js
@@ -1,1 +1,28 @@
+var d = document.getElementById("dibujito");
+var lienzo = d.getContext("2d");
+var lineas = 30;
+var l = 0;
+var yi, xf;
+var colorcito = "#FAA";
 
+for (l = 0; l < lineas; l++) 
+{
+	yi = 10 * l;
+	xf = 10 * (l + 1);
+	dibujarLinea(colorcito,0,yi,xf,300);
+	console.log("Linea" + 1);
+
+}
+
+dibujarLinea(colorcito,1,1,1,299)
+dibujarLinea(colorcito,1,299,299,299)
+
+function dibujarLinea(color,xinicial,yinicial,xfinal,yfinal)
+{
+	lienzo.beginPath();
+	lienzo.strokeStyle = color;
+	lienzo.moveTo(xinicial,yinicial);
+	lienzo.lineTo(xfinal,yfinal);
+	lienzo.stroke();
+	lienzo.closePath();
+}
